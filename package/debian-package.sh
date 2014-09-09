b515df176d3f9841acf87fc04bb21e7fbf4d7977
Christoph Kluenter <ckluente@thouthworks.com>
2014-09-09 11:38:51 +0200

    clean up debian packaging
    we use native format now. this is nice because we can skip the
    creation of the tar.gz

--- package/debian-package.sh
+++ package/debian-package.sh
@@ -25,26 +25,5 @@ else
 	git-dch -a -S 
 fi
 
-python setup.py sdist
-
-TEMP_DIR=$(mktemp -d)
-
-cp dist/pixelated-dispatcher*.tar.gz $TEMP_DIR/
-
-pushd $TEMP_DIR
-cp pixelated-dispatcher*.tar.gz pixelated-dispatcher_0.1.orig.tar.gz
-tar -xzf pixelated-dispatcher-0.1.tar.gz
-cd pixelated*
-
 dpkg-buildpackage -rfakeroot -uc -us
-
-# manual build
-#mkdir pkg-root
-#python setup.py install --root=pkg-root --install-layout=deb
-#fakeroot dpkg --build pkg-root ../python-pixelated-dispatcher_0.1-1_all.deb
-
-cp $TEMP_DIR/python-pixelated-dispatcher*all.deb /tmp/
-
-popd
-rm -Rf $TEMP_DIR
 
