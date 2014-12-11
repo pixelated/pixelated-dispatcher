pixelated/pixelated-dispatcher

commit 901d8e431a5f0665ff89078d3e646b68b39cb3ce

Added docker image download.
- Issue #39
- Image gets downloaded if image name contains a slash
- Downloading the image is now the default.
  https://registry.hub.docker.com/u/pixelated/pixelated-user-agent

pixelated/provider/docker/__init__.py
@@ -128,34 +128,53 @@ class DockerProvider(BaseProvider):
         imgs = self._docker.images()
         found = False
         for img in imgs:
-            if '%s:latest' % self._adapter.app_name() in img['RepoTags']:
+            if '%s:latest' % self._adapter.docker_image_name() in img['RepoTags']:
                 found = True
 
         if not found:
             # build the image
             start = time.time()
             logger.info('No docker image for %s found! Triggering build.' % self._adapter.app_name())
-            if pkg_resources.resource_exists('pixelated.resources', 'init-%s-docker-context.sh' % self._adapter.app_name()):
-                fileobj = None
-                content = pkg_resources.resource_string('pixelated.resources', 'init-%s-docker-context.sh' % self._adapter.app_name())
-                with TempDir() as dir:
-                    filename = join(dir, 'run.sh')
-                    with open(filename, 'w') as fd:
-                        fd.write(content)
-                        fd.close()
-                        os.chmod(filename, stat.S_IRWXU)
-                        subprocess.call([filename], cwd=dir)
-                    path = dir
-                    self._build_image(path, fileobj)
+            if '/' in self._adapter.docker_image_name():
+                self._download_image(self._adapter.docker_image_name())
             else:
-                fileobj = io.StringIO(self._dockerfile())
-                path = None
-                self._build_image(path, fileobj)
-            logger.info('Finished image %s build in %d seconds' % ('%s:latest' % self._adapter.app_name(), time.time() - start))
-        self._initializing = False
+                if pkg_resources.resource_exists('pixelated.resources', 'init-%s-docker-context.sh' % self._adapter.app_name()):
+                    fileobj = None
+                    content = pkg_resources.resource_string('pixelated.resources', 'init-%s-docker-context.sh' % self._adapter.app_name())
+                    with TempDir() as dir:
+                        filename = join(dir, 'run.sh')
+                        with open(filename, 'w') as fd:
+                            fd.write(content)
+                            fd.close()
+                            os.chmod(filename, stat.S_IRWXU)
+                            subprocess.call([filename], cwd=dir)
+                        path = dir
+                        self._build_image(path, fileobj)
+                else:
+                    fileobj = io.StringIO(self._dockerfile())
+                    path = None
+                    self._build_image(path, fileobj)
+                logger.info('Finished image %s build in %d seconds' % ('%s:latest' % self._adapter.docker_image_name(), time.time() - start))
+            self._initializing = False
+
+    def _download_image(self, docker_image_name):
+        stream = self._docker.pull(repository=docker_image_name, tag='latest', stream=True)
+        lines = []
+        for event in stream:
+            data = json.loads(event)
+            if 'status' in data:
+                logger.debug(data['status'])
+                lines.append(data['status'])
+            if 'error' in data:
+                logger.error('Failed to pull image %s: %s' % (docker_image_name, data['error']))
+                logger.error('Replaying docker pull output')
+                for line in lines:
+                    logger.error('Docker output: %s' % line)
+                logger.error('Terminating process by sending TERM signal')
+                os.kill(os.getpid(), signal.SIGTERM)
 
     def _build_image(self, path, fileobj):
-        stream = self._docker.build(path=path, fileobj=fileobj, tag='%s:latest' % self._adapter.app_name())
+        stream = self._docker.build(path=path, fileobj=fileobj, tag='%s:latest' % self._adapter.docker_image_name())
         lines = []
         for event in stream:
             data = json.loads(event)
@@ -194,7 +213,7 @@ class DockerProvider(BaseProvider):
         cm = self._map_container_by_name(all=True)
         if name not in cm:
             self._setup_instance(user_config, cm)
-            c = self._docker.create_container(self._adapter.app_name(), self._adapter.run_command(), name=name, volumes=['/mnt/user'], ports=[self._adapter.port()], environment=self._adapter.environment('/mnt/user'), stdin_open=True)
+            c = self._docker.create_container(self._adapter.docker_image_name(), self._adapter.run_command(), name=name, volumes=['/mnt/user'], ports=[self._adapter.port()], environment=self._adapter.environment('/mnt/user'), stdin_open=True)
         else:
             c = cm[name]
         data_path = self._data_path(user_config)

pixelated/test/provider/docker/docker_provider_test.py
@@ -88,7 +88,8 @@ class DockerProviderTest(unittest.TestCase):
         self.users = MagicMock(spec=Users)
         self._tmpdir = TempDir()
         self.root_path = self._tmpdir.name
-        self._adapter = PixelatedDockerAdapter()
+        self._adapter = MagicMock(wraps=PixelatedDockerAdapter())
+        self._adapter.docker_image_name.return_value = 'pixelated'
 
     def tearDown(self):
         self._tmpdir.dissolve()
@@ -112,7 +113,20 @@ class DockerProviderTest(unittest.TestCase):
         client.build.assert_called_once_with(path=None, fileobj=StringIOMatcher(dockerfile), tag='pixelated:latest')
 
     @patch('pixelated.provider.docker.docker.Client')
-    def test_initialize_skips_image_build_if_available(self, docker_mock):
+    def test_initialize_downloads_docker_image_if_image_name_contains_slash(self, docker_mock):
+        # given
+        client = docker_mock.return_value
+        client.images.return_value = []
+        self._adapter.docker_image_name.return_value = 'pixelated/pixelated-user-agent'
+
+        # when
+        DockerProvider(self._adapter, 'leap_provider', 'some leap ca', 'some docker url').initialize()
+        # then
+        docker_mock.assert_called_once_with(base_url='some docker url')
+        client.pull.assert_called_once_with(tag='latest', repository='pixelated/pixelated-user-agent', stream=True)
+
+    @patch('pixelated.provider.docker.docker.Client')
+    def test_initialize_skips_image_build_or_download_if_already_available(self, docker_mock):
         # given
         client = docker_mock.return_value
         client.images.return_value = [{'Created': 1404833111, 'VirtualSize': 297017244, 'ParentId': '57885511c8444c2b89743bef8b89eccb65f302b2a95daa95dfcc9b972807b6db', 'RepoTags': ['pixelated:latest'], 'Id': 'b4f10a2395ab8dfc5e1c0fae26fa56c7f5d2541debe54263105fe5af1d263189', 'Size': 181956643}]
@@ -134,6 +148,32 @@ class DockerProviderTest(unittest.TestCase):
             return []
 
         client.build.side_effect = build
+        provider = DockerProvider(self._adapter, 'some provider', 'some provider ca', 'some docker url')
+
+        self.assertTrue(provider.initializing)
+
+        # when
+        t = Thread(target=provider.initialize)  # move to thread so that initializing behaviour is observable
+        t.start()
+
+        # then
+        sleep(0.1)
+        self.assertTrue(provider.initializing)
+        t.join()
+        self.assertFalse(provider.initializing)
+
+    @patch('pixelated.provider.docker.docker.Client')
+    def test_reports_initializing_while_initialize_is_running_and_image_downloaded(self, docker_mock):
+        # given
+        client = docker_mock.return_value
+        client.images.return_value = []
+        self._adapter.docker_image_name.return_value = 'pixelated/pixelated-user-agent'
+
+        def download(repository, tag, stream):
+            sleep(0.2)
+            return []
+
+        client.pull.side_effect = download
         provider = DockerProvider(self._adapter, 'some provider', 'some provider ca', 'some docker url')
 
         self.assertTrue(provider.initializing)

pixelated/provider/docker/adapter.py
@@ -21,6 +21,9 @@ class DockerAdapter(object):
     def app_name(self):
         raise NotImplementedError
 
+    def docker_image_name(self):
+        return self.app_name()
+
     def run_command(self):
         raise NotImplementedError
 

pixelated/provider/docker/pixelated_adapter.py
@@ -29,6 +29,9 @@ class PixelatedDockerAdapter(DockerAdapter):
     def app_name(self):
         return 'pixelated'
 
+    def docker_image_name(self):
+        return 'pixelated/pixelated-user-agent'
+
     def run_command(self):
         return '/bin/bash -l -c "/usr/bin/pixelated-user-agent --host 0.0.0.0 --port 4567 --dispatcher-stdin"'
 

commit 24e182ed9ca14ba19b5ab9501047e632fe6c12e7

Fixed problem with always initializing provider.
- Bug introduced in 901d8e431a5f0665ff89078d3e646b68b39cb3ce
- See issue #39

pixelated/provider/docker/__init__.py
@@ -155,7 +155,7 @@ class DockerProvider(BaseProvider):
                     path = None
                     self._build_image(path, fileobj)
                 logger.info('Finished image %s build in %d seconds' % ('%s:latest' % self._adapter.docker_image_name(), time.time() - start))
-            self._initializing = False
+        self._initializing = False
 
     def _download_image(self, docker_image_name):
         stream = self._docker.pull(repository=docker_image_name, tag='latest', stream=True)

pixelated/test/provider/docker/docker_provider_test.py
@@ -130,12 +130,14 @@ class DockerProviderTest(unittest.TestCase):
         # given
         client = docker_mock.return_value
         client.images.return_value = [{'Created': 1404833111, 'VirtualSize': 297017244, 'ParentId': '57885511c8444c2b89743bef8b89eccb65f302b2a95daa95dfcc9b972807b6db', 'RepoTags': ['pixelated:latest'], 'Id': 'b4f10a2395ab8dfc5e1c0fae26fa56c7f5d2541debe54263105fe5af1d263189', 'Size': 181956643}]
+        provider = DockerProvider(self._adapter, 'leap_provider', 'some docker url')
 
         # when
-        DockerProvider(self._adapter, 'leap_provider', 'some docker url').initialize()
+        provider.initialize()
 
         # then
         self.assertFalse(client.build.called)
+        self.assertFalse(provider.initializing)
 
     @patch('pixelated.provider.docker.docker.Client')
     def test_reports_initializing_while_initialize_is_running(self, docker_mock):