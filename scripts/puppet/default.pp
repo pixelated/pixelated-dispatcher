ba4061c4cc5599f6d94d702a5d1d9bd1cf5d7d13
Jefferson Stachelski <jstachel@thoughtworks.com>
2015-04-10 17:27:15 -0300

    Removed provider-ca parameter on puppet script to run the pixelated-dispatcher manager because the try environment are using the official certificate

--- scripts/puppet/default.pp
+++ scripts/puppet/default.pp
@@ -87,7 +87,7 @@ node default {
     mode => '0755'
   }
 
-  $manager_cmd = "/usr/bin/python /vagrant/pixelated-dispatcher.py manager -b docker --provider try.pixelated-project.org --provider-ca /vagrant/pixelated/resources/try.pixelated-project.org.ca.crt --bind 0.0.0.0"
+  $manager_cmd = "/usr/bin/python /vagrant/pixelated-dispatcher.py manager -b docker --provider try.pixelated-project.org --bind 0.0.0.0"
   $ssl_options = "--sslcert /vagrant/pixelated/test/util/server.crt --sslkey /vagrant/pixelated/test/util/server.key"
 
   service { 'dispatcher-manager':
